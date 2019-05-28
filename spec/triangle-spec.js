37b74202b8670938b85ae238269a12fc0498ef1b
Student <example@example.com>
2019-05-28 16:30:28 -0700

    Changed triangle.js to be a class instead of a function - ALL TESTS FAIL CURRENTLY

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -6,20 +6,20 @@ describe('Triangle', function() {
     expect(triangle.sideB).toEqual(4);
     expect(triangle.sideC).toEqual(5);
   });
-  it('should correctly determine whether 3 lengths make up a triangle', function() {
-    var notTriangle = new Triangle(3,9,22);
-    expect(notTriangle.checkType()).toEqual("These values do not make up a triangle");
-  });
-  it('should correctly determine whether 3 sides make up an equilateral triangle', function() {
-    var eqTriangle = new Triangle(5,5,5);
-    expect(eqTriangle.checkType()).toEqual("This is an equilatoral triangle.");
-  });
-  it('should correctly determine whether 3 sides make up an isosceles triangle', function() {
-    var eqTriangle = new Triangle(5,5,9);
-    expect(eqTriangle.checkType()).toEqual("This is an isosceles triangle.");
-  });
-  it('should correctly determine whether 3 sides make up a scalene triangle', function() {
-    var eqTriangle = new Triangle(3,4,5);
-    expect(eqTriangle.checkType()).toEqual("This is a scalene triangle.");
-  });
+  // it('should correctly determine whether 3 lengths make up a triangle', function() {
+  //   var notTriangle = new Triangle(3,9,22);
+  //   expect(notTriangle.checkType()).toEqual("These values do not make up a triangle");
+  // });
+  // it('should correctly determine whether 3 sides make up an equilateral triangle', function() {
+  //   var eqTriangle = new Triangle(5,5,5);
+  //   expect(eqTriangle.checkType()).toEqual("This is an equilatoral triangle.");
+  // });
+  // it('should correctly determine whether 3 sides make up an isosceles triangle', function() {
+  //   var eqTriangle = new Triangle(5,5,9);
+  //   expect(eqTriangle.checkType()).toEqual("This is an isosceles triangle.");
+  // });
+  // it('should correctly determine whether 3 sides make up a scalene triangle', function() {
+  //   var eqTriangle = new Triangle(3,4,5);
+  //   expect(eqTriangle.checkType()).toEqual("This is a scalene triangle.");
+  // });
 });
--- src/triangle.js
+++ src/triangle.js
@@ -1,19 +1,40 @@
-export function Triangle (sideA, sideB, sideC) {
+// export function Triangle (sideA, sideB, sideC) {
+//   this.sideA = sideA;
+//   this.sideB = sideB;
+//   this.sideC = sideC;
+// }
+
+// Triangle.prototype.checkType = function() {
+//   var outputString;
+//   if ((this.sideA > (this.sideB + this.sideC)) || (this.sideB > (this.sideA + this.sideC)) || (this.sideC > (this.sideA + this.sideB))) {
+//     outputString = "These values do not make up a triangle";
+//   } else if (this.sideA === this.sideB && this.sideB === this.sideC) {
+//     outputString = "This is an equilatoral triangle.";
+//   } else if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC){
+//     outputString = "This is an isosceles triangle.";
+//   } else {
+//     outputString = "This is a scalene triangle.";
+//   }
+//   return outputString;
+// }
+
+class Triangle {
+  constructor(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
   this.sideC = sideC;
-}
-
-Triangle.prototype.checkType = function() {
-  var outputString;
-  if ((this.sideA > (this.sideB + this.sideC)) || (this.sideB > (this.sideA + this.sideC)) || (this.sideC > (this.sideA + this.sideB))) {
-    outputString = "These values do not make up a triangle";
-  } else if (this.sideA === this.sideB && this.sideB === this.sideC) {
-    outputString = "This is an equilatoral triangle.";
-  } else if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC){
-    outputString = "This is an isosceles triangle.";
-  } else {
-    outputString = "This is a scalene triangle.";
   }
-  return outputString;
+  checkType() {
+    var outputString;
+    if ((this.sideA > (this.sideB + this.sideC)) || (this.sideB > (this.sideA + this.sideC)) || (this.sideC > (this.sideA + this.sideB))) {
+      outputString = "These values do not make up a triangle";
+    } else if (this.sideA === this.sideB && this.sideB === this.sideC) {
+      outputString = "This is an equilatoral triangle.";
+    } else if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC){
+      outputString = "This is an isosceles triangle.";
+    } else {
+      outputString = "This is a scalene triangle.";
+    }
+    return outputString;
+  }
 }
